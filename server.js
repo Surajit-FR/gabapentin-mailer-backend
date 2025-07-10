9fdbc147686752f22a47b98e90af79c0a3011a0a
Surajit-FR <surojit.pal@ariprodesigns.com>
2025-07-10 12:02:02 +0530

    mail error

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 var transport = {
   name: 'gabapentin',
-  host: 'https://medspro.us',
+  host: 'medspro.us:2096',
   port: 2096, // e.g. smtp.gmail.com
   secure: true,
   auth: {
